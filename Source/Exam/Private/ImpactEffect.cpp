4359efe6f3fc890da2e151efc0fae98d27d553d5
tanakatanaka <aiuro2003haru@gmail.com>
2019-09-15 19:38:26 +0900

    impact実装

--- Source/Exam/Private/ImpactEffect.cpp
+++ Source/Exam/Private/ImpactEffect.cpp
@@ -1,27 +1,89 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include"../Exam.h"
 #include "ImpactEffect.h"
+#include "Sound/SoundCue.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+#include "Runtime/Engine/Classes/PhysicalMaterials/PhysicalMaterial.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+#include "Runtime/Engine/Classes/Materials/Material.h"
 
 // Sets default values
 AImpactEffect::AImpactEffect()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	bAutoDestroyWhenFinished = true;
 	PrimaryActorTick.bCanEverTick = true;
 
+	DecalLifeSpan = 10.0f;
+	DecalSize = 16.0f;
 }
 
-// Called when the game starts or when spawned
-void AImpactEffect::BeginPlay()
+
+
+void AImpactEffect::PostInitializeComponents()
 {
-	Super::BeginPlay();
-	
+	//Super::PostInitializeComponents();
+
+	/* Figure out what we hit (SurfaceHit is setting during actor instantiation in weapon class) */
+	UPhysicalMaterial* HitPhysMat = SurfaceHit.PhysMaterial.Get();
+	EPhysicalSurface HitSurfaceType = UPhysicalMaterial::DetermineSurfaceType(HitPhysMat);
+
+	UParticleSystem* ImpactFX = GetImpactFX(HitSurfaceType);
+	if (ImpactFX)
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(this, ImpactFX, GetActorLocation(), GetActorRotation());
+	}
+
+	USoundCue* ImpactSound = GetImpactSound(HitSurfaceType);
+	if (ImpactSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation());
+	}
+
+	if (DecalMaterial)
+	{
+		FRotator RandomDecalRotation = SurfaceHit.ImpactNormal.Rotation();
+		RandomDecalRotation.Roll = FMath::FRandRange(-180.0f, 180.0f);
+
+		UGameplayStatics::SpawnDecalAttached(DecalMaterial, FVector(DecalSize, DecalSize, 1.0f),
+			SurfaceHit.Component.Get(), SurfaceHit.BoneName,
+			SurfaceHit.ImpactPoint, RandomDecalRotation, EAttachLocation::KeepWorldPosition,
+			DecalLifeSpan);
+	}
 }
 
-// Called every frame
-void AImpactEffect::Tick(float DeltaTime)
+
+UParticleSystem* AImpactEffect::GetImpactFX(EPhysicalSurface SurfaceType) const
 {
-	Super::Tick(DeltaTime);
-
+	switch (SurfaceType)
+	{
+	case SURFACE_DEFAULT:
+		return DefaultFX;
+	case SURFACE_FLESH:
+		return PlayerFleshFX;
+	case SURFACE_ZOMBIEBODY:
+	case SURFACE_ZOMBIEHEAD:
+	case SURFACE_ZOMBIELIMB:
+		return ZombieFleshFX;
+	default:
+		return nullptr;
+	}
 }
 
+
+USoundCue* AImpactEffect::GetImpactSound(EPhysicalSurface SurfaceType) const
+{
+	switch (SurfaceType)
+	{
+	case SURFACE_DEFAULT:
+		return DefaultSound;
+	case SURFACE_FLESH:
+		return PlayerFleshSound;
+	case SURFACE_ZOMBIEBODY:
+	case SURFACE_ZOMBIEHEAD:
+	case SURFACE_ZOMBIELIMB:
+		return ZombieFleshSound;
+	default:
+		return nullptr;
+	}
+}
--- Source/Exam/Public/ImpactEffect.h
+++ Source/Exam/Public/ImpactEffect.h
@@ -6,21 +6,52 @@
 #include "GameFramework/Actor.h"
 #include "ImpactEffect.generated.h"
 
+class USoundCue;
+class UMaterial;
+
+
 UCLASS()
 class EXAM_API AImpactEffect : public AActor
 {
 	GENERATED_BODY()
-	
 public:	
 	// Sets default values for this actor's properties
 	AImpactEffect();
 
+	virtual void PostInitializeComponents() override;
+
+	/* FX spawned on standard materials */
+	UPROPERTY(EditDefaultsOnly)
+	UParticleSystem* DefaultFX;
+
+	UPROPERTY(EditDefaultsOnly)
+	UParticleSystem* PlayerFleshFX;
+
+	UPROPERTY(EditDefaultsOnly)
+	UParticleSystem* ZombieFleshFX;
+
+	UPROPERTY(EditDefaultsOnly)
+	USoundCue* DefaultSound;
+
+	UPROPERTY(EditDefaultsOnly)
+	USoundCue* PlayerFleshSound;
+
+	UPROPERTY(EditDefaultsOnly)
+	USoundCue* ZombieFleshSound;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Decal")
+	UMaterial* DecalMaterial;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Decal")
+	float DecalSize;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Decal")
+	float DecalLifeSpan;
+
+	FHitResult SurfaceHit;
+
+
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+	UParticleSystem* GetImpactFX(EPhysicalSurface SurfaceType) const;
+	USoundCue* GetImpactSound(EPhysicalSurface SurfaceType) const;
 };
